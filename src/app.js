65bb8325baa6ab217192ecc3c4223200372453e6
GOKUL <gokulmp0307@gmail.com>
2024-02-26 10:35:09 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/",(req,res) =>{
+    res.send("Hello world")
+});
+
+
+app.listen(port,()=>{
+    console.log("Server is listening"+port);
+});
